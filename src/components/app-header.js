c1aa465c603c72c6832897a87a8a1d64826174ae
bvooztaunn <antzub@ya.ru>
2020-03-12 20:13:02 +0300

    Add separate components

---
+++ src/components/app-header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AppHeader = () => {
+    return <h1>My Todo app</h1>
+};
+
+export default AppHeader;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TodoList = () => {
-    const items = ['Learn React', 'Build Awesome App']
-    return (
-        <ul>
-            <li>{ items[0] }</li>
-            <li>{ items[1] }</li>
-        </ul>
-    );
-};
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
-const AppHeader = () => {
-    return <h1>My Todo app</h1>
-};
-
-const SearchPanel = () => {
-    const searchText = 'Type here to search';
-    const searchStyle = {
-        fontSize: '25px'
-    };
-    return <input
-     searchStyle = {searchStyle}
-     placeholder = {searchText} />
-};
 
 const App = () => {
 
